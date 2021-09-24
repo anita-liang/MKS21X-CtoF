4f5e33a275c24f77a24618753ede2eccb1f6db80
Anita Liang <aliang30@stuy.edu>
2021-09-24 05:23:00 -0400

    Realized I was over complicating everything by using scanner and decided to use set test values instead.

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,34 +1,30 @@
 // A) What should the functions have as parameter(s) ? What type should they be?
 // Ans: For the parameters, I just put whatever is going to be converted for
-// each respectively. I used float bc the decimal points would
+// each respectively. I used double  bc the decimal points would
 //allow for a more accurate conversion.
 
 // B) What should the functions return?
 // The functions should return the conversion.
 
-import java.util.Scanner;
-public class celsiusToFahrenheit()
-{
-  public static void main (String args []) {
-    double celcius;
-    double fahrenheit;
-    Scanner in = new Scanner(System.in);
-    System.out.println("Converting Celcius to Fahrenheit:");
-    celcius = in.nextDouble();
-    fahrenheit = ((1.8 * celcius) + 32);
-    System.out.println("Conversion: "+fahrenheit);
-  }
+public class CtoFTester {
+  public static void main(String[] args){
+    double first = 5; //test 1
+    double second = 41; //test 2
+    System.out.println(celsiusToFahrenheit(5));
+    System.out.println(celsiusToFahrenheit(3));
+    System.out.println(fahrenheitToCelsius(5));
+    System.out.println(fahrenheitToCelsius(3));
 }
 
 
-class fahrenheitToCelsius()
-public static void main (StringSecond args []) {
-    double celcius;
-    double fahrenheit;
-    Scanner in = new Scanner(System.in);
-    System.out.println("Converting Fahrenheit to Celcius:");
-    fahrenheit = in.nextDouble();
-    celcius = (fahrenheit - 32) * 0.555555556;
-    System.out.println("Conversion: "+celcius);
+
+  public static double celsiusToFahrenheit (double celcius){
+    double fahrenheit =  ((1.8 * celcius) + 32);
+    return fahrenheit;
   }
+
+  public static double fahrenheitToCelsius (double fahrenheit){
+    double celcius = (fahrenheit - 32) * 0.555555556;
+    return celcius;
+}
 }
